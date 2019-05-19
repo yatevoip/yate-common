48c911ce86c9c4da76ce1a8c36d1f6a68150e64e
paulc <paulc@36d2ae33-d233-0510-bbc4-cff6078fbd04>
2019-05-19 11:11:45 +0000

    Added script to prepare SCTP configuration for telephony signaling.
    git-svn-id: http://voip.null.ro/svn/yate-common@110 36d2ae33-d233-0510-bbc4-cff6078fbd04

---
+++ scripts/sctp_sysctl.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+# Prepare a system SCTP configuration suitable for SIGTRAN and Diameter
+# Particularly M2PA is very sensitive to retransmission timings
+
+if ! grep -q -i 'sctp' /etc/modules 2>/dev/null; then
+    cat <<-EOF >>/etc/modules
+	# load SCTP early so sysctl can alter parameters at boot
+	sctp
+	EOF
+    modprobe sctp
+fi
+if ! grep -q -i 'sctp' /etc/sysctl.conf 2>/dev/null; then
+    cat <<-EOF >>/etc/sysctl.conf
+	# SCTP parameters more suitable for telephony
+	net.sctp.rto_min = 200
+	net.sctp.rto_max = 800
+	net.sctp.rto_initial = 200
+	net.sctp.hb_interval = 20000
+	EOF
+    sysctl -p
+fi
